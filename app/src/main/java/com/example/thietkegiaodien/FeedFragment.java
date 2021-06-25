b7524c2cc79c4026f711478c89309cfa066580c7
quangthuan01 <thuannqIt01@gmail.com>
2021-06-25 08:29:06 +0700

    Navigation,Bottom Nav, TabLayout,ViewPager, Music

---
+++ app/src/main/java/com/example/thietkegiaodien/FeedFragment.java
@@ -0,0 +1,20 @@
+package com.example.thietkegiaodien;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FeedFragment extends Fragment {
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup mview = (ViewGroup) inflater.inflate(R.layout.fragment_feed,container,false);
+
+        return mview;
+    }
+}
